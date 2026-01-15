099c4f35202ed30a861fcf0c173b59e5697b260d
Tirath Pathak <tirath.pathak@manektech.com>
2026-01-15 20:13:07 +0530

    added header

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import Image from "next/image";
-import Logo from "@/assets/subscribly.png";
+import Logo from "@/assets/subscribly.svg";
+import { MoveRight } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex items-center justify-between">
-      <Image src={Logo} alt="Logo" />
-      <div>
+      <Image src={Logo} alt="Logo" width={150} height={50} priority />
+      <div className="flex items-center gap-20">
         <div> Features</div>
         <div> Pricing</div>
       </div>
-      <div>
+      <div className="flex items-center gap-14">
         <div>Login</div>
-        <button>Getting Started</button>
+        <button className="rounded-4xl border px-2 items-center flex gap-2">
+          
+          Getting Started <MoveRight className="animate-arrow" size={20} />{" "}
+        </button>
       </div>
     </div>
   );
