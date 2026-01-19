5af6a161f672a50abccd42b37c538b89b7382062
Tirath Pathak <tirath.pathak@manektech.com>
2026-01-19 18:52:38 +0530

    changes

--- app/_components/hero/page.tsx
+++ app/_components/hero/page.tsx
@@ -1,12 +1,17 @@
-
 const HeroSection = () => {
-    return (
-        <div className="flex items-center align-middle flex-col">
-            <div>Subscription Made Simple</div>
-            <div>Take Control of Your Subscriptions</div>
-            <div>Subscribly helps you track, organize, and manage subscriptions in one central dashboard. Avoid forgotten renewals, gain full cost transparency, and stay in control of recurring expenses</div>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center align-middle flex-col mt-15">
+      <div className="rounded-2xl border-2 px-2  text-2xl">
+        Subscription Made Simple
+      </div>
+      <div>Take Control of Your Subscriptions</div>
+      <div>
+        Subscribly helps you track, organize, and manage subscriptions in one
+        central dashboard. Avoid forgotten renewals, gain full cost
+        transparency, and stay in control of recurring expenses
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
